865f215cfe0e60a054a6149de13b62258fd464b5
Alex Budovski <alexbud@microsoft.com>
2015-08-14 15:04:54 -0700

    lua: redirect getc.
    This needed special handling on the host side to serve one character at a
    time.
    
    getc is used by io.read() with no args. (calls read_line)

--- lua-5.3.1/src/luaredir.c
+++ lua-5.3.1/src/luaredir.c
@@ -7,6 +7,9 @@ lua_StdioRedir g_StdioCallbacks;
 
 #undef fprintf
 #undef fwrite
+#undef fread
+#undef fgets
+#undef getc
 
 int lua_fprintf(FILE* const stream, const char* const fmt, ...)
 {
@@ -62,6 +65,60 @@ size_t lua_fwrite(const void* buf, size_t size, size_t count, FILE* stream)
 exit:
         
    return ret; 
+}
+
+size_t lua_fread(void* buf, size_t size, size_t count, FILE* stream)
+{
+    size_t ret = 0;
+    
+    /* Don't redirect for stdout/err streams, or if no callback registered. */
+	if (!g_StdioCallbacks.cb_input || stream != stdin)
+	{
+		ret = fread(buf, size, count, stream);
+		goto exit;
+	}
+
+    assert(size == 1);
+    
+    g_StdioCallbacks.cb_input(buf, count);
+exit:
+    return ret;
+}
+
+char* lua_fgets(char* str, int num, FILE* stream)
+{
+    char* ret = NULL;
+    
+    /* Don't redirect for stdout/err streams, or if no callback registered. */
+    if (!g_StdioCallbacks.cb_input || stream != stdin)
+    {
+        ret = fgets(str, num, stream);
+        goto exit;
+    }
+
+    g_StdioCallbacks.cb_input(str, num);
+
+    ret = str;
+    
+exit:
+    return ret;
+}
+
+int lua_getc(FILE* stream)
+{
+    int ret = 0;
+    
+    /* Don't redirect for stdout/err streams, or if no callback registered. */
+    if (!g_StdioCallbacks.cb_getc || stream != stdin)
+    {
+        ret = getc(stream);
+        goto exit;
+    }
+
+    ret = g_StdioCallbacks.cb_getc();
+    
+exit:
+    return ret;
 }
 
 void lua_set_stdio_callbacks(const lua_StdioRedir* callbacks)
--- lua-5.3.1/src/luaredir.h
+++ lua-5.3.1/src/luaredir.h
@@ -18,31 +18,28 @@ typedef enum lua_OutputType
 
 typedef void (*LUA_OUTPUTCB) (lua_OutputType out_type, const char* buf);
 typedef void (*LUA_INPUTCB) (char* buf, size_t len);
-
-#if 0
-typedef int (*LUA_FPUTS) (const char* msg, FILE* stream);
-typedef char* (*LUA_FGETS) (char* msg, int maxToRead, FILE* stream);
-typedef size_t (*LUA_FREAD) (void* buf, size_t, size_t, FILE* stream);
-#endif
+typedef int (*LUA_GETCCB) ();
 
 typedef struct lua_StdioRedir
 {
 	LUA_OUTPUTCB cb_output;
 	LUA_INPUTCB cb_input;
+	LUA_GETCCB cb_getc;
 } lua_StdioRedir;
 
 /* Redefine stdio APIs that Lua calls */
 #define fprintf	lua_fprintf
 #define fwrite	lua_fwrite
-
-#if 0
-#define fputs	lua_fputs
 #define fgets	lua_fgets
 #define fread	lua_fread
-#endif
+#define getc	lua_getc
 
 LUA_API int lua_fprintf(FILE* const stream, const char* const fmt, ...);
 LUA_API size_t lua_fwrite(const void* buf, size_t size, size_t count, FILE* stream);
+
+LUA_API size_t lua_fread(void* buf, size_t size, size_t count, FILE* stream);
+LUA_API char* lua_fgets(char* str, int num, FILE* stream);
+LUA_API int lua_getc(FILE* stream);
 
 LUA_API void lua_set_stdio_callbacks(const lua_StdioRedir* callbacks);
 
